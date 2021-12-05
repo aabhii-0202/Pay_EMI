b6e38e47d80e066001298d20742facb307749153
Abhishek Singh <abhisheksingh1911a5@gmail.com>
2021-12-05 17:02:44 +0530

    pay EMI

---
+++ app/src/main/java/com/mediustechnologies/payemi/activities/payEMI.java
@@ -0,0 +1,16 @@
+package com.mediustechnologies.payemi.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mediustechnologies.payemi.R;
+
+public class payEMI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_emi);
+    }
+}
--- app/src/main/java/com/mediustechnologies/payemi/activities/paymentInfo.java
+++ app/src/main/java/com/mediustechnologies/payemi/activities/paymentInfo.java
@@ -1,7 +1,10 @@
 package com.mediustechnologies.payemi.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,6 +14,7 @@ import com.mediustechnologies.payemi.databinding.ActivityPaymentInfoBinding;
 public class paymentInfo extends AppCompatActivity {
 
     private ActivityPaymentInfoBinding binding;
+    private Context context = this;
 
 
     @Override
@@ -20,6 +24,7 @@ public class paymentInfo extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.backButton.setOnClickListener(view -> finish());
+        binding.payNow.setOnClickListener(view -> startActivity(new Intent(context,payEMI.class)));
 
 
 
