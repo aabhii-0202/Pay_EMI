3a396348c27c26c4efa87d94193fa29324091e77
Abhishek Singh <abhisheksingh1911a5@gmail.com>
2021-12-08 20:58:30 +0530

    transaction_Search

--- app/src/main/java/com/mediustechnologies/payemi/activities/transaction_Search.java
+++ app/src/main/java/com/mediustechnologies/payemi/activities/transaction_Search.java
@@ -1,15 +1,20 @@
 package com.mediustechnologies.payemi.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 
+import com.mediustechnologies.payemi.commons.DatePickerFragment;
 import com.mediustechnologies.payemi.databinding.ActivityTaransactionSearchBinding;
 
-public class transaction_Search extends AppCompatActivity {
+public class transaction_Search extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     private ActivityTaransactionSearchBinding binding;
+    private boolean from;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,17 @@ public class transaction_Search extends AppCompatActivity {
 
         init();
 
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+        String s = date+"-"+month+"-"+year;
+        if(from){
+            binding.fromtext.setText(s);
+        }else {
+            binding.totext.setText(s);
+        }
 
     }
 
@@ -42,6 +58,18 @@ public class transaction_Search extends AppCompatActivity {
         });
 
         binding.search.setOnClickListener(view -> binding.details.setVisibility(View.VISIBLE));
+
+        binding.layoutfrom.setOnClickListener(View ->{
+            from = true;
+            DialogFragment datepicker = new DatePickerFragment();
+            datepicker.show(getSupportFragmentManager(),"date picker");
+        });
+
+        binding.layoutto.setOnClickListener(view -> {
+            from = false;
+            DialogFragment datepicker = new DatePickerFragment();
+            datepicker.show(getSupportFragmentManager(),"date picker");
+        });
 
     }
 }
