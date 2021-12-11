150202e1075c85779ff628f82861ba9468fbb38e
Abhishek Singh <abhisheksingh1911a5@gmail.com>
2021-12-11 23:54:18 +0530

    otp

--- app/src/main/java/com/mediustechnologies/payemi/activities/login/signIn.java
+++ app/src/main/java/com/mediustechnologies/payemi/activities/login/signIn.java
@@ -2,18 +2,30 @@ package com.mediustechnologies.payemi.activities.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.mediustechnologies.payemi.databinding.ActivitySignInBinding;
 
 public class signIn extends AppCompatActivity {
     private ActivitySignInBinding binding;
+    private final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        init();
+
+
+
+    }
+
+    private void init(){
+        binding.sendOTPbtn.setOnClickListener(view->{startActivity(new Intent(context,verifyNumber.class));});
 
 
 
---
+++ app/src/main/java/com/mediustechnologies/payemi/activities/login/verifyNumber.java
@@ -0,0 +1,32 @@
+package com.mediustechnologies.payemi.activities.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.mediustechnologies.payemi.activities.BankList;
+import com.mediustechnologies.payemi.databinding.ActivityVerifyNumberBinding;
+
+public class verifyNumber extends AppCompatActivity {
+
+    private ActivityVerifyNumberBinding binding;
+    private final Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityVerifyNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        init();
+
+    }
+
+    private void init(){
+        binding.back.setOnClickListener(view -> finish());
+        binding.verifyOTP.setOnClickListener(view -> startActivity(new Intent(context, BankList.class)));
+    }
+
+}
