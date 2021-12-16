5d56e56248e582cd7460e22b330c5249e78a7872
Abhishek Singh <abhisheksingh1911a5@gmail.com>
2021-12-16 19:12:52 +0530

    payEMI_transaction_page_recyclerview

--- app/src/main/java/com/mediustechnologies/payemi/activities/payEMI_transaction_page.java
+++ app/src/main/java/com/mediustechnologies/payemi/activities/payEMI_transaction_page.java
@@ -1,15 +1,26 @@
 package com.mediustechnologies.payemi.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.os.Bundle;
 
+import com.mediustechnologies.payemi.Models.transaction_chat;
+import com.mediustechnologies.payemi.adapters.transction_chatAdapter;
 import com.mediustechnologies.payemi.databinding.ActivityPayEmiTransactionPageBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class payEMI_transaction_page extends AppCompatActivity {
     private Context context = this;
     private ActivityPayEmiTransactionPageBinding binding;
+    private ArrayList<transaction_chat> chatlist;
+    private transction_chatAdapter adapter;
+    private LinearLayoutManager linearLayoutManager;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +34,29 @@ public class payEMI_transaction_page extends AppCompatActivity {
     }
 
     private void dummydata(){
+        chatlist = new ArrayList<>();
+        chatlist.add(new transaction_chat("Payment to HDFC Bank","Car Loan","₹ 14,288","  Paid | 15 Oct"));
+        chatlist.add(new transaction_chat("Scratch Now","Earn a reward!","",null));
+        chatlist.add(new transaction_chat("Payment to HDFC Bank","Car Loan","₹ 14,288","  Paid | 15 Oct"));
+        chatlist.add(new transaction_chat("Payment to HDFC Bank","Car Loan","₹ 14,288","  Paid | 15 Oct"));
+        chatlist.add(new transaction_chat("Rs. 140","You earned a reward!","",null));
+        chatlist.add(new transaction_chat("Payment to HDFC Bank","Car Loan","₹ 14,288","  Paid | 15 Oct"));
+        chatlist.add(new transaction_chat("Payment to HDFC Bank","Car Loan","₹ 14,288","  Paid | 15 Oct"));
+        chatlist.add(new transaction_chat("Scratch Now","Earn a reward!","",null));
+        chatlist.add(new transaction_chat("Payment to HDFC Bank","Car Loan","₹ 14,288","  Paid | 15 Oct"));
+        chatlist.add(new transaction_chat("Payment to HDFC Bank","Car Loan","₹ 14,288","  Paid | 15 Oct"));
+        chatlist.add(new transaction_chat("Rs. 140","You earned a reward!","",null));
+        chatlist.add(new transaction_chat("Payment to HDFC Bank","Car Loan","₹ 14,288","  Paid | 15 Oct"));
 
     }
 
     private void initrecyclerview(){
+        recyclerView = binding.paymentRecyclerView;
+        linearLayoutManager = new LinearLayoutManager(context);
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        adapter = new transction_chatAdapter(context,chatlist);
+        recyclerView.setAdapter(adapter);
 
     }
 
