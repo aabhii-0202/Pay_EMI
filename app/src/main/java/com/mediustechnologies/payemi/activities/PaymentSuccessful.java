f7e6fb6ea52010c41f0d75f6d9a1dfb27f4ae4e7
Abhishek Singh <abhisheksingh1911a5@gmail.com>
2022-01-18 02:13:26 +0530

    screen shot error

--- app/src/main/java/com/mediustechnologies/payemi/activities/PaymentSuccessful.java
+++ app/src/main/java/com/mediustechnologies/payemi/activities/PaymentSuccessful.java
@@ -2,15 +2,25 @@ package com.mediustechnologies.payemi.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -18,6 +28,7 @@ import android.widget.Toast;
 
 import com.mediustechnologies.payemi.ApiResponse.RedeemScratchCard;
 import com.mediustechnologies.payemi.ApiResponse.getCashback;
+import com.mediustechnologies.payemi.BuildConfig;
 import com.mediustechnologies.payemi.DTO.billFetchDTO;
 import com.mediustechnologies.payemi.R;
 import com.mediustechnologies.payemi.activities.payments.act35payment_Page;
@@ -29,6 +40,10 @@ import com.mediustechnologies.payemi.commons.utils;
 import com.mediustechnologies.payemi.databinding.ActivityPaymentSuccessfulBinding;
 import com.mediustechnologies.payemi.helper.RetrofitClient;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -42,6 +57,13 @@ public class PaymentSuccessful extends AppCompatActivity {
     private final Context context = this;
     private String cashback,bill_id,profile_id;
     private boolean scratched;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+
+    private static final String[] PERMISSION_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+    };
+
 
 
     @Override
@@ -160,6 +182,8 @@ public class PaymentSuccessful extends AppCompatActivity {
                     billFetchDTO data = response.body().get(0);
                     setData(data);
 
+
+
                     scratch();
                 }
                 else{
@@ -257,6 +281,7 @@ public class PaymentSuccessful extends AppCompatActivity {
                         Log.d("tag","Redeemed cashback successfull");
                         Toast.makeText(context,"Cashback Added Successfully",Toast.LENGTH_LONG);
 
+
                     }
                 }
                 else if(response.code()==400){
@@ -274,10 +299,90 @@ public class PaymentSuccessful extends AppCompatActivity {
     }
 
     private void init() {
+        binding.share.setVisibility(View.VISIBLE);
         scratched = false;
         bill_id = getIntent().getStringExtra("bill_id");
         profile_id = getIntent().getStringExtra("profile_id");
         binding.crossButton.setOnClickListener(view -> nextScreen());
+        binding.share.setOnClickListener(View -> verifyStoragePermission(PaymentSuccessful.this));
+    }
+
+    private void share(){
+        ;
+
+    }
+
+    private void takeScreenShot(View view) {
+
+        //This is used to provide file name with Date a format
+        Date date = new Date();
+        CharSequence format = DateFormat.format("MM-dd-yyyy_hh:mm:ss", date);
+
+        //It will make sure to store file to given below Directory and If the file Directory dosen't exist then it will create it.
+        try {
+            File mainDir = new File(
+                    this.getExternalFilesDir(Environment.DIRECTORY_PICTURES), "FilShare");
+            if (!mainDir.exists()) {
+                boolean mkdir = mainDir.mkdir();
+            }
+
+            //Providing file name along with Bitmap to capture screenview
+            String path = mainDir + "/" + "PayEMI" + "-" + format + ".jpeg";
+            view.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+            view.setDrawingCacheEnabled(false);
+
+            //This logic is used to save file at given location with the given filename and compress the Image Quality.
+            File imageFile = new File(path);
+            FileOutputStream fileOutputStream = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+
+            //Create New Method to take ScreenShot with the imageFile.
+            shareScreenShot(imageFile);
+        } catch (IOException e) {
+           Log.e("tag","Exception in taking screen shot."+e.toString());
+        }
+    }
+
+    //Share ScreenShot
+    private void shareScreenShot(File imageFile) {
+
+        //Using sub-class of Content provider
+        Uri uri = FileProvider.getUriForFile(
+                context,
+                BuildConfig.APPLICATION_ID + "." + getLocalClassName() + ".provider",
+                imageFile);
+
+        //Explicit intent
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("image/*");
+        intent.putExtra(android.content.Intent.EXTRA_TEXT, "Pay EMI and earn cashback.");
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+
+        //It will show the application which are available to share Image; else Toast message will throw.
+        try {
+            this.startActivity(Intent.createChooser(intent, "Share With"));
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "No App Available", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Permissions Check
+
+    private  void verifyStoragePermission(Activity activity) {
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSION_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE);
+        }else{
+            takeScreenShot(getWindow().getDecorView());
+        }
     }
 
     @Override
