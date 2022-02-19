f99a609a67e9cac2809c4a65367a6f8552f0143f
abhishek <abhisheksingh1911a5@gmail.com>
2022-02-19 16:58:58 +0530

    Profile Page done

--- app/src/main/java/com/mediustechnologies/payemi/activities/DashBoardclasses/ProfileFraggment.java
+++ app/src/main/java/com/mediustechnologies/payemi/activities/DashBoardclasses/ProfileFraggment.java
@@ -1,11 +1,13 @@
 package com.mediustechnologies.payemi.activities.DashBoardclasses;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -45,6 +47,7 @@ public class ProfileFraggment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
 
+        binding.datalayout.setVisibility(View.GONE);
         callapiforprofile();
 
 
@@ -52,7 +55,72 @@ public class ProfileFraggment extends Fragment {
 
         binding.profileupdate.setOnClickListener(view1 -> {
 
+            binding.progress.setVisibility(View.VISIBLE);
 
+            String name = binding.profilename.getText().toString();
+            String mail = binding.profilemail.getText().toString();
+            String username = binding.profileUsername.getText().toString();
+            String address = binding.profileaddress.getText().toString();
+            String imgurl = "";
+
+
+
+
+            Call<ProfileInfoResponse> call = new RetrofitClient().getInstance(context, urlconstants.AuthURL).getApi().updateProfileInfo(utils.access_token,utils.phone,name,mail,username,address,imgurl);
+
+            call.enqueue(new Callback<ProfileInfoResponse>() {
+                @Override
+                public void onResponse(Call<ProfileInfoResponse> call, Response<ProfileInfoResponse> response) {
+
+                    binding.progress.setVisibility(View.GONE);
+
+                    if(response.code()==utils.RESPONSE_SUCCESS&&response.body()!=null) {
+                        if (response.body().getError() == null || response.body().getError().equalsIgnoreCase("false")) {
+
+                            try {
+                                binding.profilephone.setText(utils.phone);
+                                binding.profilename.setText(utils.name);
+                                binding.profilemail.setText(response.body().getData().get(0).getEmail());
+                                binding.profileUsername.setText(response.body().getData().get(0).getUser());
+                                binding.profileaddress.setText(response.body().getData().get(0).getAddress());
+
+
+                                SharedPreferences preferences = context.getApplicationContext().getSharedPreferences("MY_APP", Context.MODE_PRIVATE);
+
+                                preferences.edit().putString("name",response.body().getData().get(0).getUser_name()).apply();
+                                preferences.edit().putString("profileid", response.body().getData().get(0).getId()).apply();
+                                preferences.edit().putString("cutomerid", response.body().getData().get(0).getCustomer_id()).apply();
+
+                                Toast.makeText(context, "Profile Updated", Toast.LENGTH_SHORT).show();
+
+                            }
+                            catch (Exception e){
+                                e.printStackTrace();
+
+                            }
+
+
+
+                        } else {
+                            try {
+                                utils.errortoast(context, response.body().getMessage());
+                            } catch (Exception e) {
+                                Log.e("tag", e.toString());
+                            }
+                        }
+                    }
+                    else {
+                        Log.e("tag",""+response.code());
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ProfileInfoResponse> call, Throwable t) {
+                    binding.progress.setVisibility(View.GONE);
+                    Log.e("tag",t.getMessage());
+                    Toast.makeText(context, "Error fetching data", Toast.LENGTH_SHORT).show();
+                }
+            });
 
 
 
@@ -64,11 +132,16 @@ public class ProfileFraggment extends Fragment {
 
     private void callapiforprofile() {
 
+        binding.progress.setVisibility(View.VISIBLE);
+
         Call<ProfileInfoResponse> call = new RetrofitClient().getInstance(context, urlconstants.AuthURL).getApi().profileInfo(utils.access_token,utils.phone);
 
         call.enqueue(new Callback<ProfileInfoResponse>() {
             @Override
             public void onResponse(Call<ProfileInfoResponse> call, Response<ProfileInfoResponse> response) {
+
+                binding.progress.setVisibility(View.GONE);
+
                 if(response.code()==utils.RESPONSE_SUCCESS&&response.body()!=null) {
                     if (response.body().getError() == null || response.body().getError().equalsIgnoreCase("false")) {
 
@@ -76,7 +149,7 @@ public class ProfileFraggment extends Fragment {
                             binding.profilephone.setText(utils.phone);
                             binding.profilename.setText(utils.name);
                             binding.profilemail.setText(response.body().getData().get(0).getEmail());
-                            binding.profileUsername.setText(response.body().getData().get(0).getUser());
+                            binding.profileUsername.setText(response.body().getData().get(0).getUser_name());
                             binding.profileaddress.setText(response.body().getData().get(0).getAddress());
                         }
                         catch (Exception e){
@@ -84,7 +157,7 @@ public class ProfileFraggment extends Fragment {
 
                         }
 
-
+                        binding.datalayout.setVisibility(View.VISIBLE);
 
                     } else {
                         try {
@@ -94,11 +167,16 @@ public class ProfileFraggment extends Fragment {
                         }
                     }
                 }
+                else {
+                    Log.e("tag",""+response.code());
+                }
             }
 
             @Override
             public void onFailure(Call<ProfileInfoResponse> call, Throwable t) {
-
+                binding.progress.setVisibility(View.GONE);
+                Log.e("tag",t.getMessage());
+                Toast.makeText(context, "Error fetching data", Toast.LENGTH_SHORT).show();
             }
         });
 
